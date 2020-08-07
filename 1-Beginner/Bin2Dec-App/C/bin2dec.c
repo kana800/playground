36b0bcf6c49a9b78bba18b60a34246668c6d4cfa
kana800 <inuxxc@tutanota.com>
2020-08-07 12:14:15 +0530

    added solution for binary to decimal C [console]

---
+++ 1-Beginner/Bin2Dec-App/C/bin2dec.c
@@ -0,0 +1,28 @@
+// converts binary number to a decimal number
+
+#include <stdio.h>
+
+int BinaryToDecimal(int binary)
+{
+  int n =  binary;
+  int decimal = 0;
+  int base = 1;
+  int last_digit = 0;
+
+  while (n != 0)
+  {
+    last_digit = n % 10 ;
+    decimal += last_digit * base;
+    base *= 2;
+    n /= 10;
+  }
+
+  return decimal;
+}
+
+int main()
+{
+  int x = BinaryToDecimal(11101);
+  printf("Binary Number is %d\n",x);
+  return 0;
+}
