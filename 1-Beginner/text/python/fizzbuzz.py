85857cdda527acd803d37edb0f3d4d46f1a0b13d
kana800 <inuxxc@tutanota.com>
2020-08-21 14:00:03 +0530

    added solutions for text questions

--- 1-Beginner/text/python/fizzbuzz.py
+++ 1-Beginner/text/python/fizzbuzz.py
@@ -4,11 +4,9 @@ Write a program that prints the numbers from 1 to 100. But for multiples of thre
 
 def fizzbuzz():
     for i in range(1,101):
-        if i % 3 == 0: print("Fizz")
+        if i % 3 == 0 and i % 5 == 0: print("FizzBuzz")
         elif i % 5 == 0: print("Buzz")
-        elif i % 3 == 0 and i % 5 ==0: print("FizzBuzz")
+        elif i % 3 == 0: print("Fizz")
         else:
             print(i)
-
-
 fizzbuzz()
