703bc33489f9e2b1ed0746c85a5de3684894a193
kana800 <inuxxc@tutanota.com>
2020-10-16 15:14:24 +0530

    completed bin2dec app

--- 1-Beginner/Bin2Dec-App/Bash/binary2decimal.sh
+++ 1-Beginner/Bin2Dec-App/Bash/binary2decimal.sh
@@ -4,5 +4,21 @@ printf "This is program converts binary numbers to decimal\n"
 #reading the input
 read input;
 
-while input:
-  
+
+length=${#input}
+#checking for digits greater than 9
+if [[ $length -ge 9 ]] 
+then
+  echo "binary number cannot be greater than 8" 1>&2
+  exit
+fi
+
+#checking for other digits
+if [[ $input =~ [2-9] ]]
+then
+  echo "binary number cannot contain numbers other than 0 and 1" 1>&2
+  exit
+fi
+
+decimal=`echo "ibase=2;obase=A;$input" | bc`
+echo "decimal number is $decimal"
