67c78b4de1c6fc5bd05019e05e5ac03c77e86237
kana800 <fdsjkomsal@gmail.com>
2020-11-10 19:58:11 +0530

    added input validation

---
+++ 1-Beginner/input_validation/js/main.js
@@ -0,0 +1,14 @@
+// selectors
+
+username = document.getElementById("username");
+email = document.getElementById("email");
+button = document.getElementById("btn");
+
+
+button.addEventListener("clicked",(e) => {
+	e.preventDefault();
+	// function to validate the string
+	// if correct alert
+	
+
+})
