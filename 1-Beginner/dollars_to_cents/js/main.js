1ae17ee522a29663c5c1ca3d6eddb3a9f13d45ed
Rahul Bhatt <rahulbhatt1899@gmail.com>
2021-10-02 15:12:57 +0530

    Added code to convert dollar to coins with the sub-type of dollars: penny, nickel, dime and quarter.
    Added code to convert dollar to coins with the sub-type of dollars: penny, nickel, dime and quarter.

---
+++ 1-Beginner/dollars_to_cents/js/main.js
@@ -0,0 +1,35 @@
+// convert dollar to decimal and "alert" the answer
+function DollarToCents(number)
+{
+    let penny, nickel, dime, quarter, balance;
+    
+    quarter = Math.floor(number / 25);
+    balance = number % 25;
+    dime = Math.floor(balance / 10);
+    balance = balance % 10;
+    nickel = Math.floor(balance / 5);
+    balance = balance % 5;
+    penny = Math.floor(balance / 1);
+    let s = "Quarters: " + quarter + "\nDimes: " + dime + "\nNickels: " + nickel + "\nPennies: " + penny;
+    alert(s);
+    return s;
+};
+
+
+function foo()
+{
+  let dollarAmt = document.getElementById("dollar").value;
+  document.getElementById("cents").innerHTML = DollarToCents(dollarAmt);
+};
+
+function validateForm()
+{
+    let x = document.getElementById("dollar").value;
+    if (x == "")
+    {
+        alert("input is empty")
+        return false;
+    }
+    foo()
+    return true;
+};
