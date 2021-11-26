37deccce44bb263f492ae5c24d91c2f58ea2c469
kana800 <64830745+kana800@users.noreply.github.com>
2021-11-26 12:19:08 +0530

    removed study directory

--- study/projectintroduction/code/conditions.lisp
+++
@@ -1,6 +0,0 @@
-(defun abs(x)
-	(if (< x 0)
-	  (- x)
-	  x))
-
-(print(abs -5))
--- study/projectintroduction/code/exercise_1.1.x/exercise.1.2.lisp
+++
@@ -1,3 +0,0 @@
-; Translate the following into prefix form
-; https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-10.html#%_sec_1.1.7
-(/ (+ 5  1 (- 2 (- 3 (+ 6 1.5))))(* 3 (- 6 2) (- 2 7)))
